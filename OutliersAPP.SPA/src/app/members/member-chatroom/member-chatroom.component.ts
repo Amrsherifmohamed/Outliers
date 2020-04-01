2cd214ecbe383466feee3e245ddce92888170021
youssef ghonem <46647058+YoussefGhonem@users.noreply.github.com>
2020-04-01 17:37:41 +0200

    startchat room

---
+++ OutliersAPP.SPA/src/app/members/member-chatroom/member-chatroom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-chatroom',
+  templateUrl: './member-chatroom.component.html',
+  styleUrls: ['./member-chatroom.component.css']
+})
+export class MemberChatroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
